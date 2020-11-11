55b50b2b1cfd3e707bf4523848104393932e6112
Alex Barlow <a@alexb.cc>
2020-11-11 17:37:50 +0000

    Add oauth client config

--- grafana/index.ts
+++ grafana/index.ts
@@ -1,11 +1,13 @@
-import * as k8s from '@pulumi/kubernetes';
-import * as pulumi from '@pulumi/pulumi';
-import { K8SExec } from '../exec';
-import { namespace } from '../monitoring';
-import { provider, kubeconfig } from '../cluster';
-import timescale, { namespace as tsNamespace } from '../timescale';
+import * as k8s from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as gcp from "@pulumi/gcp";
+import { K8SExec } from "../exec";
+import { namespace } from "../monitoring";
+import { provider, kubeconfig } from "../cluster";
+import timescale, { namespace as tsNamespace } from "../timescale";
 
 const cf = new pulumi.Config("dply");
+const c = new pulumi.Config("gcp");
 
 export const database = new K8SExec(
   "grafana-db",
@@ -44,14 +46,10 @@ export const dbAccess = new K8SExec(
     podSelector: "role=master",
     container: "timescaledb",
     kubeConfig: kubeconfig,
-    cmd: [
-      "psql",
-      "-c",
-      `GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana`
-    ]
+    cmd: ["psql", "-c", `GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana`]
   },
   { dependsOn: [...timescale, dbUser, database] }
-)
+);
 
 export const creds = new k8s.core.v1.Secret(
   "grafana-credentials",
@@ -72,6 +70,18 @@ export const creds = new k8s.core.v1.Secret(
   { provider }
 );
 
+const brand = new gcp.iap.Brand("M3O", {
+  applicationTitle: "M3O",
+  supportEmail: "support@m3o.com"
+});
+
+const oauthClient = new gcp.iap.Client("grafana-oauth", {
+  brand: pulumi.interpolate`projects/${c.require("gcp:project")}/brands/${
+    brand.id
+  }`,
+  displayName: "M3O"
+});
+
 export const chart = new k8s.helm.v3.Chart(
   "grafana",
   {
@@ -84,18 +94,24 @@ export const chart = new k8s.helm.v3.Chart(
       adminPassword: cf.require("grafana-admin-pass"),
       sidecar: {
         datasources: {
-          enabled: true,
-        },
+          enabled: true
+        }
       },
+      "grafana.ini": {
+        "auth.google": {
+          enabled: true,
+          client_id: oauthClient.clientId,
+          client_secret: oauthClient.secret,
+          scopes:
+            "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email",
+          auth_url: "https://accounts.google.com/o/oauth2/auth",
+          token_url: "https://accounts.google.com/o/oauth2/token",
+          allow_sign_up: true
+        }
+      }
     }
   },
   { provider, dependsOn: [...timescale, dbAccess] }
 );
 
-export default [
-  database,
-  dbUser,
-  dbAccess,
-  creds,
-  chart,
-]
+export default [database, dbUser, dbAccess, creds, chart];
