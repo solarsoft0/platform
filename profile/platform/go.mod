10aafa758dc902af67ff856dfae9b341f8c527a4
Dominic Wong <dom@m3o.com>
2021-08-24 10:00:44 +0100

    Backup events store (#23)

--- profile/platform/go.mod
+++ profile/platform/go.mod
@@ -11,8 +11,8 @@ require (
 	github.com/micro/micro/plugin/redis/blocklist/v3 v3.0.0-20210818101332-9a333769a3d7
 	github.com/micro/micro/plugin/redis/broker/v3 v3.0.0-20210622092058-b12502169757
 	github.com/micro/micro/plugin/redis/stream/v3 v3.0.0-20210622092058-b12502169757
-	github.com/micro/micro/plugin/s3/v3 v3.0.0-20210804135839-3b9d79ed593c
-	github.com/micro/micro/v3 v3.3.1-0.20210818101332-9a333769a3d7
+	github.com/micro/micro/plugin/s3/v3 v3.0.0-20210824085733-d451d4c79ecd
+	github.com/micro/micro/v3 v3.4.1-0.20210824085733-d451d4c79ecd
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/prometheus/procfs v0.2.0 // indirect
 	github.com/urfave/cli/v2 v2.3.0
--- profile/platform/platform.go
+++ profile/platform/platform.go
@@ -4,6 +4,7 @@ package platform
 import (
 	"crypto/tls"
 	"os"
+	"sync"
 
 	"github.com/go-redis/redis/v8"
 	"github.com/micro/micro/plugin/postgres/v3"
@@ -41,88 +42,113 @@ func init() {
 	profile.Register("platform_client", ClientProfile)
 }
 
+var (
+	// hack because setup func is called multiple times due to before func weirdness
+	platformOnce sync.Once
+	clientOnce   sync.Once
+)
+
 // Profile is for running the micro platform
 var Profile = &profile.Profile{
 	Name: "platform",
 	Setup: func(ctx *cli.Context) error {
-		auth.DefaultAuth = jwt.NewAuth()
-		// the cockroach store will connect immediately so the address must be passed
-		// when the store is created. The cockroach store address contains the location
-		// of certs so it can't be defaulted like the broker and registry.
-		store.DefaultStore = postgres.NewStore(store.Nodes(ctx.String("store_address")))
-		profile.SetupBroker(redisBroker.NewBroker(broker.Addrs(ctx.String("broker_address"))))
-		profile.SetupRegistry(etcd.NewRegistry(registry.Addrs(ctx.String("registry_address"))))
-		profile.SetupJWT(ctx)
-		profile.SetupConfigSecretKey(ctx)
+		var retErr error
+		platformOnce.Do(func() {
+			auth.DefaultAuth = jwt.NewAuth()
+			// the cockroach store will connect immediately so the address must be passed
+			// when the store is created. The cockroach store address contains the location
+			// of certs so it can't be defaulted like the broker and registry.
+			store.DefaultStore = postgres.NewStore(store.Nodes(ctx.String("store_address")))
+			profile.SetupBroker(redisBroker.NewBroker(broker.Addrs(ctx.String("broker_address"))))
+			profile.SetupRegistry(etcd.NewRegistry(registry.Addrs(ctx.String("registry_address"))))
+			profile.SetupJWT(ctx)
+			profile.SetupConfigSecretKey(ctx)
 
-		// Set up a default metrics reporter (being careful not to clash with any that have already been set):
-		if !metrics.IsSet() {
-			prometheusReporter, err := prometheus.New()
+			// Set up a default metrics reporter (being careful not to clash with any that have already been set):
+			if !metrics.IsSet() {
+				prometheusReporter, err := prometheus.New()
+				if err != nil {
+					retErr = err
+					return
+				}
+				metrics.SetDefaultMetricsReporter(prometheusReporter)
+			}
+
+			var err error
+			if ctx.Args().Get(1) == "events" {
+				events.DefaultStream, err = redisstream.NewStream(redisStreamOpts(ctx)...)
+				if err != nil {
+					logger.Fatalf("Error configuring stream: %v", err)
+				}
+
+				opts := []s3.Option{
+					s3.Credentials(
+						os.Getenv("MICRO_EVENTS_STORE_ACCESS_KEY"),
+						os.Getenv("MICRO_EVENTS_STORE_SECRET_KEY"),
+					),
+					s3.Endpoint(os.Getenv("MICRO_EVENTS_STORE_ENDPOINT")),
+					s3.Region(os.Getenv("MICRO_EVENTS_STORE_REGION")),
+					s3.Bucket(os.Getenv("MICRO_EVENTS_STORE_BUCKET")),
+				}
+
+				events.DefaultStore = evStore.NewStore(
+					evStore.WithStore(store.DefaultStore),
+					evStore.WithBackup(s3.NewBackup(opts...)),
+				)
+
+			}
+
+			// only configure the blob store for the store and runtime services
+			if ctx.Args().Get(1) == "runtime" || ctx.Args().Get(1) == "store" {
+				opts := []s3.Option{
+					s3.Credentials(
+						os.Getenv("MICRO_BLOB_STORE_ACCESS_KEY"),
+						os.Getenv("MICRO_BLOB_STORE_SECRET_KEY"),
+					),
+					s3.Endpoint(os.Getenv("MICRO_BLOB_STORE_ENDPOINT")),
+					s3.Region(os.Getenv("MICRO_BLOB_STORE_REGION")),
+					s3.Bucket(os.Getenv("MICRO_BLOB_STORE_BUCKET")),
+				}
+				if val := os.Getenv("MICRO_BLOB_STORE_INSECURE"); len(val) > 0 {
+					opts = append(opts, s3.Insecure())
+				}
+
+				store.DefaultBlobStore, err = s3.NewBlobStore(opts...)
+				if err != nil {
+					logger.Fatalf("Error configuring s3 blob store: %v", err)
+				}
+			}
+
+			reporterAddress := os.Getenv("MICRO_TRACING_REPORTER_ADDRESS")
+			if len(reporterAddress) == 0 {
+				reporterAddress = jaeger.DefaultReporterAddress
+			}
+			// Configure tracing with Jaeger:
+			tracingServiceName := ctx.Args().Get(1)
+			if len(tracingServiceName) == 0 {
+				tracingServiceName = "Micro"
+			}
+			openTracer, _, err := jaeger.New(
+				opentelemetry.WithServiceName(tracingServiceName),
+				opentelemetry.WithTraceReporterAddress(reporterAddress),
+			)
 			if err != nil {
-				return err
+				logger.Fatalf("Error configuring opentracing: %v", err)
 			}
-			metrics.SetDefaultMetricsReporter(prometheusReporter)
-		}
+			opentelemetry.DefaultOpenTracer = openTracer
+			opentracing.SetGlobalTracer(openTracer)
 
-		var err error
-		if ctx.Args().Get(1) == "events" {
-			events.DefaultStream, err = redisstream.NewStream(redisStreamOpts(ctx)...)
+			microRuntime.DefaultRuntime = kubernetes.NewRuntime(
+				kubernetes.RuntimeClassName("kata-fc"),
+			)
+			microBuilder.DefaultBuilder, err = golang.NewBuilder()
 			if err != nil {
-				logger.Fatalf("Error configuring stream: %v", err)
-			}
-		}
-
-		// only configure the blob store for the store and runtime services
-		if ctx.Args().Get(1) == "runtime" || ctx.Args().Get(1) == "store" {
-			opts := []s3.Option{
-				s3.Credentials(
-					os.Getenv("MICRO_BLOB_STORE_ACCESS_KEY"),
-					os.Getenv("MICRO_BLOB_STORE_SECRET_KEY"),
-				),
-				s3.Endpoint(os.Getenv("MICRO_BLOB_STORE_ENDPOINT")),
-				s3.Region(os.Getenv("MICRO_BLOB_STORE_REGION")),
-				s3.Bucket(os.Getenv("MICRO_BLOB_STORE_BUCKET")),
-			}
-			if val := os.Getenv("MICRO_BLOB_STORE_INSECURE"); len(val) > 0 {
-				opts = append(opts, s3.Insecure())
+				logger.Fatalf("Error configuring golang builder: %v", err)
 			}
 
-			store.DefaultBlobStore, err = s3.NewBlobStore(opts...)
-			if err != nil {
-				logger.Fatalf("Error configuring s3 blob store: %v", err)
-			}
-		}
-
-		reporterAddress := os.Getenv("MICRO_TRACING_REPORTER_ADDRESS")
-		if len(reporterAddress) == 0 {
-			reporterAddress = jaeger.DefaultReporterAddress
-		}
-		// Configure tracing with Jaeger:
-		tracingServiceName := ctx.Args().Get(1)
-		if len(tracingServiceName) == 0 {
-			tracingServiceName = "Micro"
-		}
-		openTracer, _, err := jaeger.New(
-			opentelemetry.WithServiceName(tracingServiceName),
-			opentelemetry.WithTraceReporterAddress(reporterAddress),
-		)
-		if err != nil {
-			logger.Fatalf("Error configuring opentracing: %v", err)
-		}
-		opentelemetry.DefaultOpenTracer = openTracer
-		opentracing.SetGlobalTracer(openTracer)
-
-		microRuntime.DefaultRuntime = kubernetes.NewRuntime(
-			kubernetes.RuntimeClassName("kata-fc"),
-		)
-		microBuilder.DefaultBuilder, err = golang.NewBuilder()
-		if err != nil {
-			logger.Fatalf("Error configuring golang builder: %v", err)
-		}
-		events.DefaultStore = evStore.NewStore(evStore.WithStore(store.DefaultStore))
-
-		kubernetes.DefaultImage = "ghcr.io/m3o/cells:v3"
-		return nil
+			kubernetes.DefaultImage = "ghcr.io/m3o/cells:v3"
+		})
+		return retErr
 	},
 }
 
@@ -150,19 +176,23 @@ func redisStreamOpts(ctx *cli.Context) []redisstream.Option {
 var ClientProfile = &profile.Profile{
 	Name: "platform_client",
 	Setup: func(ctx *cli.Context) error {
-		// Set up a default metrics reporter (being careful not to clash with any that have already been set):
-		if !metrics.IsSet() {
-			prometheusReporter, err := prometheus.New()
-			if err != nil {
-				return err
+		var retErr error
+		clientOnce.Do(func() {
+			// Set up a default metrics reporter (being careful not to clash with any that have already been set):
+			if !metrics.IsSet() {
+				prometheusReporter, err := prometheus.New()
+				if err != nil {
+					retErr = err
+					return
+				}
+				metrics.SetDefaultMetricsReporter(prometheusReporter)
 			}
-			metrics.SetDefaultMetricsReporter(prometheusReporter)
-		}
-		auth2.DefaultBlockList = blocklist.New(
-			os.Getenv("MICRO_API_REDIS_ADDRESS"),
-			os.Getenv("MICRO_API_REDIS_USER"),
-			os.Getenv("MICRO_API_REDIS_PASSWORD"),
-			&tls.Config{})
-		return nil
+			auth2.DefaultBlockList = blocklist.New(
+				os.Getenv("MICRO_API_REDIS_ADDRESS"),
+				os.Getenv("MICRO_API_REDIS_USER"),
+				os.Getenv("MICRO_API_REDIS_PASSWORD"),
+				&tls.Config{})
+		})
+		return retErr
 	},
 }
